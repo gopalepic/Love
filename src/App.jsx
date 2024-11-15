8aec5d8e552216e79854dbee33ccdecee8985e07
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-11-15 11:12:22 +0530

    Ap

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className="bg-[#c98dc0] h-[20vw] flex items-center justify-center text-4xl text-[#fffefb] font-bold">
+        <h1>Our Love Story</h1>
+      </div>
+      <div className="bg-[#b38cb7] h-[20vw] text-3xl flex flex-col items-center p-4 shadow-lg text-[#fffbfe] justify-center ">
+        Will you Be Mine ?
+        <p>
+          A journey of love , memories, and a proposal that will last forever
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center  text-center h-[30vw]">
+    <h1 className="text-[#9379a0] text-5xl">Our Journey</h1>
+    <p className="text-[#726e73] w-[60vw] mt-14 ">
+
+          From the very first moment out eyes met , we knew something special
+          was in the air , we've laughed , cried and made countless memories
+          together. This website is our way of sharing our love story , our
+          adventures , and the beautiful future we are ready to build. Forever
+          starts now .
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center  text-center h-[30vw]">
+        <h1 className="text-[#9379a0] text-4xl font-semibold ">THE PROPOSAL</h1>
+        <p className="text-[#726e73]  w-[60vw] mt-14 ">As we stand here today, I can't imagine my life without you. You are my heart, my soul, and my everything. So, I ask you: Will you marry me, and spend forever with me?"</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
